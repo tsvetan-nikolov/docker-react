2af87b70dacae38e4cef8e2cec5599cfc34dc324
tsvetan-nikolov <cvetannikolovofc@gmail.com>
2023-01-20 22:12:31 +0200

    react app default message added

--- src/App.js
+++ src/App.js
@@ -9,14 +9,14 @@ function App() {
         <p>
           👋👋👋
         </p>
-        {/* <a
+        <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a> */}
+        </a>
       </header>
     </div>
   );
